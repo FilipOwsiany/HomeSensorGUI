0fed0ff22377372d62abb75e9d9179ccebc484e9
Filip Owsiany <filowsi29@gmail.com>
2025-11-01 22:33:11 +0100

    refactor(CCell): Simplify constructor parameters and improve style settings

--- src/basicWidgets/cell/CCell.cpp
+++ src/basicWidgets/cell/CCell.cpp
@@ -2,24 +2,33 @@
 
 #define DEBUG_CELL 0
 
-CCell::CCell(lv_obj_t * aParent, int32_t aWidth, int32_t aHeight, int32_t aCol, int32_t aRow)
+CCell::CCell(lv_obj_t * parent, int32_t w, int32_t h, int32_t col, int32_t row)
 {
-    mObject = lv_obj_create(aParent);
-    lv_obj_set_size(mObject, aWidth, aHeight);
-    lv_obj_set_style_radius(mObject, 0, 0);
-    lv_obj_clear_flag(mObject, LV_OBJ_FLAG_SCROLLABLE);
-    lv_obj_clear_flag(mObject, LV_OBJ_FLAG_CLICKABLE); 
+    mObject = lv_obj_create(parent);
+    lv_obj_set_size(mObject, w, h);
     lv_obj_set_grid_cell(mObject,
-        LV_GRID_ALIGN_CENTER, aCol, 1,
-        LV_GRID_ALIGN_CENTER, aRow, 1);
-
-    lv_obj_set_style_bg_opa(mObject, LV_OPA_TRANSP, 0);
-#if DEBUG_CELL == 1
-    lv_obj_set_style_border_width(mObject, 1, 0);
-    lv_obj_set_style_border_color(mObject, lv_palette_main(LV_PALETTE_RED), 0);    
-#else
+        LV_GRID_ALIGN_STRETCH, col, 1,
+        LV_GRID_ALIGN_STRETCH, row, 1);
     lv_obj_set_style_border_width(mObject, 0, 0);
-#endif
+    lv_obj_set_style_bg_opa(mObject, LV_OPA_TRANSP, 0);
+    lv_obj_set_style_pad_all(mObject, 0, 0);
+    lv_obj_set_style_pad_row(mObject, 0, 0);
+    lv_obj_set_style_pad_column(mObject, 0, 0);
+    #if DEBUG_CELL == 1
+        lv_obj_set_style_border_width(mObject, 1, 0);
+        lv_obj_set_style_radius(mObject, 0, 0);
+        lv_obj_set_style_border_color(mObject, lv_palette_main(LV_PALETTE_RED), 0);
+
+    #endif
+    lv_obj_clear_flag(mObject, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_clear_flag(mObject, LV_OBJ_FLAG_CLICKABLE);
+    // printf("[CCell] Debug style paddings:\n");
+    // printf("  pad_left   = %d\n", lv_obj_get_style_pad_left(mObject, 0));
+    // printf("  pad_right  = %d\n", lv_obj_get_style_pad_right(mObject, 0));
+    // printf("  pad_top    = %d\n", lv_obj_get_style_pad_top(mObject, 0));
+    // printf("  pad_bottom = %d\n", lv_obj_get_style_pad_bottom(mObject, 0));
+    // printf("  pad_row    = %d\n", lv_obj_get_style_pad_row(mObject, 0));
+    // printf("  pad_column = %d\n", lv_obj_get_style_pad_column(mObject, 0));
 }
 
 CCell::~CCell()
